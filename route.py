b58d5c25d8ef95b2c5f9d42c6266ecf22f3329e3
Radik <radik1506.706@gmail.com>
2025-12-03 13:49:58 +0100

    004

---
+++ route.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+class User:
+    def __init__(self,username, nom, password, email, rol):
+        self.username=username
+        self.nom=nom
+        self.password=password
+        self.email=email
+        self.rol=rol
+us1=User(username="aa", nom = "Test Test", password = "1234", email = "wqd@gm@com", rol = "Role")
+print(us1.rol)
+
+'''
+@app.route('/user/', methods=['GET'])
+def user():
+    resposta = ""
+    print("AAAAAAAAAAA")
+    username = request.args.get("Username",default="")
+    if username != "":
+        resposta = "username=", username
+    else:
+        resposta = "No username format"
+    return "test"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
